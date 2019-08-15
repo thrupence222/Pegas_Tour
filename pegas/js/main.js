5b2a46cc26da6fabc602528bdc3754443c18b963
thrupence222 <53616128+thrupence222@users.noreply.github.com>
2019-08-15 13:57:44 +0300

    Add files via upload

---
+++ pegas/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 900);
+    });
+});
+
+$('.carousel').carousel({
+            interval: 3000,
+        })
+
+
